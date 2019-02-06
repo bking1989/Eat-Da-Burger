cc518cfc39fc5ad88d31acec17ce8f63b3087574
bking1989 <bradley_king@me.com>
2019-02-05 23:28:59 -0700

    Set up our server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+// Require the Express package
+var express = require("express");
+
+// Set up our Express application
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import our routes
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start up our virtual server
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
